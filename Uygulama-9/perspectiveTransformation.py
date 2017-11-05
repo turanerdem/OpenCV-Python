21d6c864897a56360a45f8e5aedf679b1f57edb4
Turan Erdem <tesimsek@gmail.com>
2017-11-05 12:56:28 +0300

    Create perspectiveTransformation.py

---
+++ Uygulama-9/perspectiveTransformation.py
@@ -0,0 +1,13 @@
+import cv2
+img = cv2.imread('sudoku_training.png')
+
+rows,cols,ch = img.shape
+pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+
+M = cv2.getPerspectiveTransform(pts1,pts2)
+dst = cv2.warpPerspective(img,M,(300,300))
+
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
+plt.show()
